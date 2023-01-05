7d82abc7c269bc81388ca3cb1c6d4bcaef335d91
Whites-ru <air-sun@mail.ru>
2023-01-05 22:36:15 +0300

    6

--- dist/sw.js
+++ dist/sw.js
@@ -1,27 +1,27 @@
 // модуль работы Service Worker
 
-const cacheName = "app_cache_ca5f8c71-2f29-4f42-81cb-857f311f6ad2"; // имя кэша приложения
+const cacheName = "app_cache_09b887f6-968a-4357-8149-02c1355ebaea"; // имя кэша приложения
 
 // установка и открытие кэша
 self.addEventListener("install", (e) => {
     e.waitUntil(
         caches.open(cacheName).then((cache) => cache.addAll([
-            "/",
-            "index.html",
-            "manifest.json",
+            "./",
+            "./index.html",
+            "./manifest.json",
 
-            "app_bundle_ca5f8c71-2f29-4f42-81cb-857f311f6ad2.css",
-            "/images/icon_any.png",
-            "/images/icon_x48.png",
-            "/images/icon_x72.png",
-            "/images/icon_x96.png",
-            "/images/icon_x128.png",
-            "/images/icon_x192.png",
-            "/images/icon_x384.png",
-            "/images/icon_x512.png",
-            "/images/icon.png",
-            
-            "app_bundle_ca5f8c71-2f29-4f42-81cb-857f311f6ad2.js"
+            "./app_bundle_09b887f6-968a-4357-8149-02c1355ebaea.css",
+            "./images/icon_any.png",
+            "./images/icon_x48.png",
+            "./images/icon_x72.png",
+            "./images/icon_x96.png",
+            "./images/icon_x128.png",
+            "./images/icon_x192.png",
+            "./images/icon_x384.png",
+            "./images/icon_x512.png",
+            "./images/icon.png",
+
+            "./app_bundle_09b887f6-968a-4357-8149-02c1355ebaea.js"
         ])),
     );
 });
